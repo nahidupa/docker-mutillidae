a3d14565cccda28b3371e03f1918352dd468d33f
nahidul kibria <nahid@orbitax.com>
2016-02-20 20:56:15 +0600

    replace localhost to mysql connection

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #/bin/sh
-
+/bin/sed -i -e "s/localhost/$MYSQL_PORT_3306_TCP_ADDR/g" /var/www/html/mutillidae/classes/MySQLHandler.php
+/bin/sed -i -e "s/allow_url_include = Off/allow_url_include = On/g" /etc/php5/apache2/php.ini
 /usr/sbin/apache2ctl -D FOREGROUND
