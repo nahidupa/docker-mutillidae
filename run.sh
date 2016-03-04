a30a7c28122fe16bd58aa65ee551e401e76cce3c
nahidupa <nahidupa@gmail.com>
2016-03-04 08:16:13 +0600

    Update run.sh
    /var/www/html/index.html modify

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #/bin/sh
+echo "Good to go" > /var/www/html/index.html
 /bin/sed -i -e "s/localhost/$MYSQL_PORT_3306_TCP_ADDR/g" /var/www/html/mutillidae/classes/MySQLHandler.php
 /bin/sed -i -e "s/allow_url_include = Off/allow_url_include = On/g" /etc/php5/apache2/php.ini
 /usr/sbin/apache2ctl -D FOREGROUND
